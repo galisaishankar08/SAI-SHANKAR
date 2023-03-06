225f3302b4429a64bd6699f2b3f72bca72b243a2
SAI SHANKAR <80621346+galisaishankar08@users.noreply.github.com>
2023-03-06 09:28:43 +0530

    contact us off

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -45,7 +45,7 @@ const Contact = () => {
       </div>
       {/* right side form */}
       <div className="c-right">
-        <form ref={form} onSubmit={sendEmail}>
+        <form ref={form} > {/* onSubmit={sendEmail} */}
           <input type="text" name="user_name" className="user"  placeholder="Name"/>
           <input type="email" name="user_email" className="user" placeholder="Email"/>
           <textarea name="message" className="user" placeholder="Message"/>
